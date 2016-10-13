e662243a9c1c145b8e82fb255e13a728bb4f9bfa
Wu Leo <675892264@qq.com>
2016-10-14 07:35:15 +0800

    Fuck this

---
+++ product/js/index.js
@@ -0,0 +1,1 @@
+function setRem(){document.documentElement.style.fontSize=innerWidth/20+"px"}setRem(),onresize=setRem;var Close=document.querySelector(".ad-close"),Ad=document.querySelector(".ad");Close.onclick=function(){Ad.style.display="none"};
